4a5b08264a70c31d7403c7ba4583ba6f27fd80be
heiwa4126 <heiwa4126@gmail.com>
2023-07-19 13:50:28 +0900

    add README

---
+++ README.md
@@ -0,0 +1,19 @@
+# my-react-app
+
+テンプレート用
+
+2023-07に
+
+```bash
+pnpm create vite my-react-app
+```
+
+で作った Vite + React (TypeScript) に
+
+- モジュールを更新した
+- `pnpm format` でprettierでフォーマットできそうなもの全部をフォーマットする
+- .gitattributes追加
+- README.md追加
+- build時にHTMLをminify
+
+などを追加したもの。
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), ViteMinifyPlugin({})],
+  base: process.env.GITHUB_REPOSITORY?.split('/')[1] ?? './' // for GitHub Pages
 });
